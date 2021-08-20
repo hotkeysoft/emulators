31e98de49c60d1b75d2a5d17851e2a584549a81e
hotkeysoft <dev@hotkeysoft.net>
2021-08-19 22:54:29 -0400

    Bin helpers

--- 8086/Common.h
+++ 8086/Common.h
@@ -1,6 +1,22 @@
 #pragma once
 #include <cstdint>
 
+#define PRINTF_BIN_PATTERN_INT8 "%c%c%c%c%c%c%c%c"
+#define PRINTF_BYTE_TO_BIN_INT8(i)    \
+    (((i) & 0x80ll) ? '1' : '0'), \
+    (((i) & 0x40ll) ? '1' : '0'), \
+    (((i) & 0x20ll) ? '1' : '0'), \
+    (((i) & 0x10ll) ? '1' : '0'), \
+    (((i) & 0x08ll) ? '1' : '0'), \
+    (((i) & 0x04ll) ? '1' : '0'), \
+    (((i) & 0x02ll) ? '1' : '0'), \
+    (((i) & 0x01ll) ? '1' : '0')
+
+#define PRINTF_BIN_PATTERN_INT16 \
+    PRINTF_BIN_PATTERN_INT8              PRINTF_BIN_PATTERN_INT8
+#define PRINTF_BYTE_TO_BIN_INT16(i) \
+    PRINTF_BYTE_TO_BIN_INT8((i) >> 8),   PRINTF_BYTE_TO_BIN_INT8(i)
+
 namespace emul
 {
 	typedef uint8_t BYTE;
