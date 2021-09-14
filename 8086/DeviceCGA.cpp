0eeee5ce366932d29a8feea4bd2f8d8312c1fd2b
hotkeysoft <dev@hotkeysoft.net>
2021-09-13 22:07:43 -0400

    CRTC Registers

--- 8086/DeviceCGA.cpp
+++ 8086/DeviceCGA.cpp
@@ -32,25 +32,35 @@ namespace cga
 		m_charROM.LoadBinary(charROM);
 		m_charROMStart = m_charROM.getPtr8(4096 + 2048);
 
-		// Register Select
-		Connect(m_baseAddress + 4, static_cast<PortConnector::OUTFunction>(&DeviceCGA::OUT));
-		// Register Value
-		Connect(m_baseAddress + 5, static_cast<PortConnector::OUTFunction>(&DeviceCGA::OUT));
+		// CRTC Register Select
+		Connect(m_baseAddress + 0, static_cast<PortConnector::OUTFunction>(&DeviceCGA::SelectCRTCRegister));
+		Connect(m_baseAddress + 2, static_cast<PortConnector::OUTFunction>(&DeviceCGA::SelectCRTCRegister));
+		Connect(m_baseAddress + 4, static_cast<PortConnector::OUTFunction>(&DeviceCGA::SelectCRTCRegister));
+		Connect(m_baseAddress + 6, static_cast<PortConnector::OUTFunction>(&DeviceCGA::SelectCRTCRegister));
 
+		// CRTC Register Data
+		Connect(m_baseAddress + 1, static_cast<PortConnector::OUTFunction>(&DeviceCGA::WriteCRTCData));
+		Connect(m_baseAddress + 3, static_cast<PortConnector::OUTFunction>(&DeviceCGA::WriteCRTCData));
+		Connect(m_baseAddress + 5, static_cast<PortConnector::OUTFunction>(&DeviceCGA::WriteCRTCData));
+		Connect(m_baseAddress + 7, static_cast<PortConnector::OUTFunction>(&DeviceCGA::WriteCRTCData));
+
+		Connect(m_baseAddress + 1, static_cast<PortConnector::INFunction>(&DeviceCGA::ReadCRTCData));
+		Connect(m_baseAddress + 3, static_cast<PortConnector::INFunction>(&DeviceCGA::ReadCRTCData));
+		Connect(m_baseAddress + 5, static_cast<PortConnector::INFunction>(&DeviceCGA::ReadCRTCData));
+		Connect(m_baseAddress + 7, static_cast<PortConnector::INFunction>(&DeviceCGA::ReadCRTCData));
 
 		// Mode Control Register
-		Connect(m_baseAddress + 8, static_cast<PortConnector::OUTFunction>(&DeviceCGA::OUT));
-		Connect(m_baseAddress + 8, static_cast<PortConnector::INFunction>(&DeviceCGA::IN));
+		Connect(m_baseAddress + 8, static_cast<PortConnector::OUTFunction>(&DeviceCGA::WriteModeControlRegister));
 
-		// Color Control Register
-		Connect(m_baseAddress + 9, static_cast<PortConnector::OUTFunction>(&DeviceCGA::OUT));
-		Connect(m_baseAddress + 9, static_cast<PortConnector::INFunction>(&DeviceCGA::IN));
+		// Color Select Register
+		Connect(m_baseAddress + 9, static_cast<PortConnector::OUTFunction>(&DeviceCGA::WriteColorSelectRegister));
 
-		Connect(m_baseAddress + 0xA, static_cast<PortConnector::INFunction>(&DeviceCGA::ReadStatus));
+		// Status Register
+		Connect(m_baseAddress + 0xA, static_cast<PortConnector::INFunction>(&DeviceCGA::ReadStatusRegister));
 
 		SDL_Init(SDL_INIT_VIDEO);
 		SDL_CreateWindowAndRenderer(640, 480, 0, &m_sdlWindow, &m_sdlRenderer);
-		SDL_RenderSetScale(m_sdlRenderer, 1.0, 2.4);
+		SDL_RenderSetScale(m_sdlRenderer, 1.0f, 2.4f);
 	}
 
 	void DeviceCGA::Render()
@@ -69,20 +79,116 @@ namespace cga
 		}
 	}
 
-	BYTE DeviceCGA::IN()
+	void DeviceCGA::SelectCRTCRegister(BYTE value)
 	{
-		LogPrintf(Logger::LOG_DEBUG, "IN");
-		return 0;
-	}
-	void DeviceCGA::OUT(BYTE value)
-	{
-		LogPrintf(Logger::LOG_DEBUG, "OUT");
+		LogPrintf(Logger::LOG_DEBUG, "SelectCRTCRegister, reg=%d", value);
+		m_crtc.currRegister = (value > _CRT_MAX_REG) ? CRT_INVALID_REG : (CRTRegister)value;
 	}
 
-	BYTE DeviceCGA::ReadStatus()
+	BYTE DeviceCGA::ReadCRTCData()
 	{
-		LogPrintf(Logger::LOG_DEBUG, "ReadStatus, hSync=%d, vSync=%d", IsHSync(), IsVSync());
+		LogPrintf(Logger::LOG_DEBUG, "ReadCRTCData, reg=%d", m_crtc.currRegister);
+
+		switch (m_crtc.currRegister)
+		{
+		case CRT_CURSOR_ADDR_HI:
+			return (m_crtc.cursorAddress >> 8);
+		case CRT_CURSOR_ADDR_LO:
+			return (BYTE)m_crtc.cursorAddress;
+
+		case CRT_LIGHT_PEN_HI:
+		case CRT_LIGHT_PEN_LO:
+		default:
+			return 0xFF;
+		}
+	}
+	void DeviceCGA::WriteCRTCData(BYTE value)
+	{
+		switch (m_crtc.currRegister)
+		{
+		case CRT_H_TOTAL_CHAR:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:             hTotal = %d characters", value);
+			m_crtc.hTotal = value;
+			break;
+		case CRT_H_DISPLAYED_CHAR:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:         hDisplayed = %d characters", value);
+			m_crtc.hDisplayed = value;
+			break;
+		case CRT_H_SYNC_POS_CHAR:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:           hSyncPos = %d characters", value);
+			m_crtc.hSyncPos = value;
+			break;
+		case CRT_H_SYNC_WIDTH_CHAR:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:         hSyncWidth = %d characters", value);
+			m_crtc.hSyncWidth = value;
+			break;
+		case CRT_V_TOTAL_ROW:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:             vTotal = %d rows", value);
+			m_crtc.vTotal = value;
+			break;
+		case CRT_V_TOTAL_ADJ_LINES:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:       vTotalAdjust = %d scanlines", value);
+			m_crtc.vTotalAdjust = value;
+			break;
+		case CRT_V_DISPLAYED_ROW:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:    vTotalDisplayed = %d rows", value);
+			m_crtc.vTotalDisplayed = value;
+			break;
+		case CRT_V_SYNC_POS_ROW:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:           vSyncPos = %d rows", value);
+			m_crtc.vSyncPos = value;
+			break;
+		case CRT_INTERLACE_MODE:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:      interlaceMode = %d", value);
+			m_crtc.interlaceMode = value;
+			break;
+		case CRT_MAX_SCANLINE_ADDR:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData: maxScanlineAddress = %d scanlines", value);
+			m_crtc.maxScanlineAddress = value;
+			break;
+		case CRT_CURSOR_START_LINE:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:        cursorStart = %d scanline" , value);
+			m_crtc.cursorStart = value;
+			break;
+		case CRT_CURSOR_END_LINE:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:          cursorEnd = %d scanline", value);
+			m_crtc.cursorEnd = value;
+			break;
+		case CRT_START_ADDR_HI:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:   startAddress(HI) = %d", value);
+			emul::SetHiByte(m_crtc.startAddress, value);
+			break;
+		case CRT_START_ADDR_LO:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:  startAddress(LOW) = %d", value);
+			emul::SetLowByte(m_crtc.startAddress, value);
+			break;
+		case CRT_CURSOR_ADDR_HI:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:  cursorAddress(HI) = %d", value);
+			emul::SetHiByte(m_crtc.cursorAddress, value);
+			break;
+		case CRT_CURSOR_ADDR_LO:
+			LogPrintf(Logger::LOG_INFO, "WriteCRTCData:  cursorAddress(LO) = %d", value);
+			emul::SetLowByte(m_crtc.cursorAddress, value);
+			break;
+		default:
+			LogPrintf(Logger::LOG_WARNING, "WriteCRTCData: Invalid Register, reg=%d", m_crtc.currRegister);
+		}
+	}
+
+	BYTE DeviceCGA::ReadStatusRegister()
+	{
+		LogPrintf(Logger::LOG_DEBUG, "ReadStatusRegister, hSync=%d, vSync=%d", IsHSync(), IsVSync());
 		return (IsHSync() ? 1 : 0) | (IsVSync() ? 8 : 0);
+	}
+
+	void DeviceCGA::WriteModeControlRegister(BYTE value)
+	{
+		LogPrintf(Logger::LOG_DEBUG, "WriteModeControlRegister, value=%02Xh", value);
+	}
+
+	void DeviceCGA::WriteColorSelectRegister(BYTE value)
+	{
+		LogPrintf(Logger::LOG_DEBUG, "WriteColorSelectRegister, value=%02Xh", value);
 	}
 
 	void DeviceCGA::Tick()
--- 8086/DeviceCGA.h
+++ 8086/DeviceCGA.h
@@ -30,11 +30,6 @@ namespace cga
 
 		void Tick();
 
-		BYTE IN();
-		void OUT(BYTE value);
-
-		BYTE ReadStatus();
-
 		emul::MemoryBlock& GetVideoRAM() { return m_screenB800; }
 
 	protected:
@@ -43,8 +38,69 @@ namespace cga
 		bool IsHSync() { return m_hPos > 640; }
 		bool IsVSync() { return m_vPos > 200; }
 
+		// CRT Controller
+		enum CRTRegister
+		{
+			CRT_H_TOTAL_CHAR =		0x0, // WRITE
+			CRT_H_DISPLAYED_CHAR =	0x1, // WRITE
+			CRT_H_SYNC_POS_CHAR =	0x2, // WRITE
+			CRT_H_SYNC_WIDTH_CHAR = 0x3, // WRITE
+			CRT_V_TOTAL_ROW =		0x4, // WRITE
+			CRT_V_TOTAL_ADJ_LINES = 0x5, // WRITE
+			CRT_V_DISPLAYED_ROW =	0x6, // WRITE
+			CRT_V_SYNC_POS_ROW =	0x7, // WRITE
+			CRT_INTERLACE_MODE =	0x8, // WRITE
+			CRT_MAX_SCANLINE_ADDR = 0x9, // WRITE
+			CRT_CURSOR_START_LINE = 0xA, // WRITE
+			CRT_CURSOR_END_LINE =	0xB, // WRITE
+			CRT_START_ADDR_HI =		0xC, // WRITE
+			CRT_START_ADDR_LO =		0xD, // WRITE
+			CRT_CURSOR_ADDR_HI =	0xE, // READ/WRITE
+			CRT_CURSOR_ADDR_LO =	0xF, // READ/WRITE
+			CRT_LIGHT_PEN_HI =		0x10,// READ
+			CRT_LIGHT_PEN_LO =		0x11,// READ
+			
+			_CRT_MAX_REG = CRT_LIGHT_PEN_LO,
+			CRT_INVALID_REG = 0xFF
+		};
+
+		struct CRTCData
+		{
+			CRTRegister currRegister = CRT_INVALID_REG;
+
+			BYTE hTotal = 0;
+			BYTE hDisplayed = 0;
+			BYTE hSyncPos = 0;
+			BYTE hSyncWidth = 0;
+			BYTE vTotal = 0;
+			BYTE vTotalAdjust = 0;
+			BYTE vTotalDisplayed = 0;
+			BYTE vSyncPos = 0;
+			BYTE interlaceMode = 0;
+			BYTE maxScanlineAddress = 0;
+			BYTE cursorStart = 0;
+			BYTE cursorEnd = 0;
+			WORD startAddress = 0;
+			WORD cursorAddress = 0;
+		} m_crtc;
+
+		void SelectCRTCRegister(BYTE value);
+		BYTE ReadCRTCData();
+		void WriteCRTCData(BYTE value);
+
 		WORD m_hPos = 0;
 		WORD m_vPos = 0;
+
+		// Mode Control Register
+		BYTE m_modeControlRegister = 0;
+		void WriteModeControlRegister(BYTE value);
+
+		// Color Select Register
+		BYTE m_colorSelectRegister = 0;
+		void WriteColorSelectRegister(BYTE value);
+
+		// Status Register
+		BYTE ReadStatusRegister();
 
 		// 16K screen buffer
 		emul::MemoryBlock m_screenB800;
