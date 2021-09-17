cb88ba884dc585e6dc4858c60a1e96f77e8c785b
hotkeysoft <dev@hotkeysoft.net>
2021-09-17 17:20:24 -0400

    8259 registers

--- 8086/Device8259.cpp
+++ 8086/Device8259.cpp
@@ -11,37 +11,205 @@ namespace pic
 
 	void Device8259::Reset()
 	{
-
+		m_state = STATE::UNINITIALIZED;
 	}
 
 	void Device8259::Init()
 	{
-		Connect(m_baseAddress, static_cast<PortConnector::INFunction>(&Device8259::IN));
-		Connect(m_baseAddress, static_cast<PortConnector::OUTFunction>(&Device8259::OUT));
+		Connect(m_baseAddress, static_cast<PortConnector::INFunction>(&Device8259::Read0));
+		Connect(m_baseAddress, static_cast<PortConnector::OUTFunction>(&Device8259::Write0));
 
-		Connect(m_baseAddress+1, static_cast<PortConnector::INFunction>(&Device8259::Mask_IN));
-		Connect(m_baseAddress+1, static_cast<PortConnector::OUTFunction>(&Device8259::Mask_OUT));
-	}
-
-	BYTE Device8259::IN()
-	{
-		LogPrintf(LOG_DEBUG, "IN");
-		return 0;
-	}
-	void Device8259::OUT(BYTE value)
-	{
-		LogPrintf(LOG_DEBUG, "OUT, value=%02X", value);
+		Connect(m_baseAddress+1, static_cast<PortConnector::INFunction>(&Device8259::Read1));
+		Connect(m_baseAddress+1, static_cast<PortConnector::OUTFunction>(&Device8259::Write1));
 	}
 
-	BYTE Device8259::Mask_IN()
+	BYTE Device8259::Read0()
 	{
-		LogPrintf(LOG_INFO, "GET mask=%02X", m_mask);
-		return m_mask;
+		LogPrintf(LOG_DEBUG, "Read0, value=%02X", *m_reg0);
+
+		return *m_reg0;
 	}
-	void Device8259::Mask_OUT(BYTE value)
+	void Device8259::Write0(BYTE value)
 	{
-		LogPrintf(LOG_INFO, "PUT mask=%02X", value);
-		m_mask = value;
+		LogPrintf(LOG_DEBUG, "Write0, value=%02X", value);
+
+		if (value & 16)
+		{
+			LogPrintf(LOG_INFO, "ICW1: Begin initialization sequence");
+			m_interruptMaskRegister = 0;
+			m_reg0 = &m_interruptRequestRegister;
+
+			m_init.icw4Needed = (value & 1);
+			LogPrintf(LOG_INFO, "ICW1: ICW4 needed: %d", m_init.icw4Needed);
+			if (!m_init.icw4Needed)
+			{
+				throw std::exception("ICW1: icw4Needed == false not supported");
+			}
+
+			m_init.single = (value & 2);
+			LogPrintf(LOG_INFO, "ICW1: SINGLE mode: %d", m_init.single);
+			if (!m_init.single)
+			{
+				throw std::exception("ICW1: cascade mode not supported");
+			}
+
+			m_init.levelTriggered = (value & 8);
+			LogPrintf(LOG_INFO, "ICW1: %s trigger mode", m_init.levelTriggered ? "LEVEL" : "EDGE");
+			if (m_init.levelTriggered)
+			{
+				throw std::exception("ICW1: LEVEL trigger mode supported");
+			}
+
+			m_state = STATE::ICW2;
+		} 
+		else if (m_state == STATE::READY)
+		{
+			switch (value & 0b00011000)
+			{
+			case 0: OCW2(value); break;
+			case 0b00001000: OCW3(value); break;
+			default:
+				LogPrintf(LOG_ERROR, "Write0: Invalid value %d", value);
+				throw std::exception("Write0: Invalid value ");
+			}
+		}
+		else
+		{
+			LogPrintf(LOG_ERROR, "Write0: Invalid state %d", m_state);
+			throw std::exception("Write0: Invalid state");
+		}
+
+	}
+
+	void Device8259::OCW2(BYTE value)
+	{
+		switch (value >> 5)
+		{
+		case 0b001:
+			LogPrintf(LOG_INFO, "OCW2: Non-specific EOI");
+			EOI();
+			break;
+
+		case 0b011:
+			LogPrintf(LOG_ERROR, "OCW2: Specific EOI, not implemented");
+			break;
+
+		case 0b101:
+		case 0b100:
+		case 0b000:
+		case 0b111:
+			LogPrintf(LOG_ERROR, "OCW2: Rotate, not implemented");
+			break;
+
+		case 0b110:
+			LogPrintf(LOG_ERROR, "OCW2: Set Priority, not implemented");
+			break;
+
+		case 0b010:
+			LogPrintf(LOG_INFO, "OCW2: No operation");
+			break;
+		}
+	}
+	void Device8259::OCW3(BYTE value)
+	{
+		switch (value & 3)
+		{
+		case 0b00:
+		case 0b01:
+			// No action
+			break;
+		case 0b10:
+			LogPrintf(LOG_INFO, "OCW3: Read Interrupt Request (IR) Register");
+			m_reg0 = &m_interruptRequestRegister;
+			break;
+		case 0b11:
+			LogPrintf(LOG_INFO, "OCW3: Read In Service (IS) Register");
+			m_reg0 = &m_inServiceRegister;
+			break;
+		}
+	}
+
+	void Device8259::EOI()
+	{
+	}
+
+
+	BYTE Device8259::Read1()
+	{
+		LogPrintf(LOG_DEBUG, "Read1: value=%02X", m_interruptMaskRegister);
+
+		switch (m_state)
+		{
+		case STATE::READY:
+			LogPrintf(LOG_INFO, "OCW1: Read Mask: %02Xh", m_interruptMaskRegister);
+			return m_interruptMaskRegister;
+		default:
+			LogPrintf(LOG_ERROR, "Read1: Invalid state %d", m_state);
+			throw std::exception("Read1: Invalid state");
+		}
+
+		return 0xFF;
+	}
+
+	void Device8259::Write1(BYTE value)
+	{
+		LogPrintf(LOG_DEBUG, "Write1: value=%02X", value);
+
+		switch (m_state)
+		{
+		case STATE::ICW2:
+			// Assume 8086 mode, ICW2 contains upper 4 bits of 
+			// interrupt # to send to CPU
+			m_init.interruptBase = value & 0b11111000;
+			LogPrintf(LOG_INFO, "ICW2: Interrupt Base: %02Xh", m_init.interruptBase);
+
+			m_state = m_init.single ? (m_init.icw4Needed ? STATE::ICW4 : STATE::READY) : STATE::ICW3;
+			break;
+		case STATE::ICW3:
+			throw std::exception("ICW3: not supported");
+			break;
+		case STATE::ICW4:
+			m_init.cpu8086 = (value & 1);
+			LogPrintf(LOG_INFO, "ICW4: CPU Mode: %s", m_init.cpu8086 ? "8086" : "8080");
+			if (!m_init.cpu8086)
+			{
+				throw std::exception("ICW4: cpu8086 == false not supported");
+			}
+
+			m_init.autoEOI = (value & 2);
+			LogPrintf(LOG_INFO, "ICW4: %s EOI", m_init.autoEOI ? "AUTO" : "NORMAL");
+			if (m_init.autoEOI)
+			{
+				throw std::exception("ICW4: autoEOI == true not supported");
+			}
+
+			// D2 = master slave, not used
+
+			m_init.buffered = (value & 8);
+			LogPrintf(LOG_INFO, "ICW4: Buffered mode: %d", m_init.buffered);
+			if (!m_init.buffered)
+			{
+				throw std::exception("ICW4: Non buffered mode not supported");
+			}
+
+			m_init.sfnm = (value & 16);
+			LogPrintf(LOG_INFO, "ICW4: Special Fully Nested mode: %d", m_init.sfnm);
+			if (m_init.sfnm)
+			{
+				throw std::exception("ICW4: Special Fully Nested mode not supported");
+			}
+			m_state = STATE::READY;
+			break;
+
+		case STATE::READY:
+			LogPrintf(LOG_INFO, "OCW1: Set Mask: %02Xh", value);
+			m_interruptMaskRegister = value;
+			break;
+		default:
+			LogPrintf(LOG_ERROR, "Write1: Invalid state %d", m_state);
+			throw std::exception("Write1: Invalid state");
+		}
+
 	}
 
 	void Device8259::Tick()
--- 8086/Device8259.h
+++ 8086/Device8259.h
@@ -21,20 +21,57 @@ namespace pic
 		Device8259(Device8259&&) = delete;
 		Device8259& operator=(Device8259&&) = delete;
 
+		void InterruptRequest(BYTE interrupt);
+
 		void Init();
 		void Reset();
 
 		void Tick();
 
-		BYTE IN();
-		void OUT(BYTE value);
-
-		BYTE Mask_IN();
-		void Mask_OUT(BYTE value);
+		BYTE GetMask() { return m_interruptMaskRegister; }
 
 	protected:
 		const WORD m_baseAddress;
 
-		BYTE m_mask;
+		enum class STATE
+		{
+			UNINITIALIZED,
+
+			ICW1,
+			ICW2,
+			ICW3,
+			ICW4,
+			
+			READY,
+		} m_state = STATE::UNINITIALIZED;
+
+		struct INIT
+		{
+			bool icw4Needed = false;
+			bool single = false; // 1 = single, 0 = cascade
+			bool levelTriggered = false; // 1 = level trigger, 0 = edge trigger
+			BYTE interruptBase = 0; // Upper 5 bits of interrupt sent to cpu
+			bool cpu8086 = false; // 1 = 8086/8088, 0 = 8080/8085
+			bool autoEOI = false; // 1 = Auto EOI (End of Interrupt), 0 = Normal EOI
+			bool buffered = false; // 1 = Buffered Mode
+			bool sfnm = false; // 1 = Special Fully Nested Mode
+		} m_init;
+
+		BYTE Read0();
+		void Write0(BYTE value);
+
+		BYTE Read1();
+		void Write1(BYTE value);
+
+		void OCW2(BYTE value);
+		void OCW3(BYTE value);
+		
+		void EOI();
+
+		BYTE m_interruptRequestRegister = 0;
+		BYTE m_inServiceRegister = 0;
+		BYTE m_interruptMaskRegister = 0;
+
+		BYTE* m_reg0 = &m_inServiceRegister; // points to IR or IS depending on last OCW3 command
 	};
 }
