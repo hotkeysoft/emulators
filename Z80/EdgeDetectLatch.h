3d293e26e67cf018ea1057661f5d98cdf1243ffe
hotkeysoft <dev@hotkeysoft.net>
2022-10-21 13:19:53 -0400

    Latch bugfix, serialization
    (temp, to be put in Common folder)

--- Z80/EdgeDetectLatch.h
+++ Z80/EdgeDetectLatch.h
@@ -1,14 +1,21 @@
 #pragma once
 
+#ifndef HSCOMMON_NO_SERIALIZE
+#include <Serializable.h>
+#endif
+
 namespace hscommon
 {
 	class EdgeDetectLatch
+#ifndef HSCOMMON_NO_SERIALIZE
+		: public emul::Serializable
+#endif
 	{
 	public:
 		// POSITIVE: Latches on 0->1 transition
 		// NEGATIVE: Latches on 1->0 transition
 		enum class Trigger { POSITIVE, NEGATIVE };
-		EdgeDetectLatch(Trigger t = Trigger::POSITIVE) : m_invert(t == Trigger::NEGATIVE) {}
+		EdgeDetectLatch(Trigger t = Trigger::POSITIVE) { SetTrigger(t); }
 
 		void Set(bool set)
 		{
@@ -21,9 +28,26 @@ namespace hscommon
 			m_value = set;
 		}
 
-		void ResetLatch() { m_latched = false; }
+		void ResetLatch() { if (m_latched) { m_latched = false; m_value = true; } }
 		bool IsLatched() const { return m_latched; }
 
+		void SetTrigger(Trigger t) { ResetLatch(); m_invert = (t == Trigger::NEGATIVE); }
+
+#ifndef HSCOMMON_NO_SERIALIZE
+		// emul::Serializable
+		virtual void Serialize(json& to) override
+		{
+			to["invert"] = m_invert;
+			to["value"] = m_value;
+			to["latched"] = m_latched;
+		}
+		virtual void Deserialize(const json& from) override
+		{
+			m_invert = from["invert"];
+			m_value = from["value"];
+			m_latched = from["latched"];
+		}
+#endif
 	private:
 		bool m_invert = false;
 		bool m_value = true;
