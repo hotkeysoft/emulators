18dbb7c1f034452db1f442fd78076656599e5529
hotkeysoft <dev@hotkeysoft.net>
2023-08-14 19:16:01 -0400

    ToUpper/Lower

--- Common/StringUtil.h
+++ Common/StringUtil.h
@@ -1,10 +1,34 @@
 #pragma once
 
 #include <string_view>
+#include <string>
+#include <algorithm>
 
 class StringUtil
 {
 public:
+    static std::string& ToUpper(std::string& str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+        return str;
+    }
+    static std::string ToUpper(std::string str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+        return str;
+    }
+
+    static std::string& ToLower(std::string& str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        return str;
+    }
+    static std::string ToLower(std::string str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        return str;
+    }
+
     static bool EndsWith(std::string_view str, std::string_view suffix)
     {
         return (str.size() >= suffix.size()) && (str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0);
